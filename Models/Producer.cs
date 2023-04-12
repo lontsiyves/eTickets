cd51919bb5e96322e7b1d14aabe649ab3c8a80ca
DANIEL LONTSI <daniel.lontsi@businessone.io>
2023-04-12 16:26:11 +0100

    model

---
+++ Models/Producer.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Producer
+    {
+
+
+        [key]
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+
+        public string ProfilePictureURL { get; set; }
+    }
+}
