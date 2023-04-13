bc233e78eeb88e065056685a4c1fbe8b3c1192f6
DANIEL LONTSI <daniel.lontsi@businessone.io>
2023-04-13 12:23:17 +0100

    update movie

--- Models/Cinema.cs
+++ Models/Cinema.cs
@@ -10,10 +10,13 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name= "Cinema Name")]
         public string Name { get; set; }
 
-        public string Description { get; set; } 
+        [Display(Name = "Description")]
+        public string Description { get; set; }
 
+        [Display(Name = "Cinema Logo")]
         public string logo { get; set; }
 
         public List<Movie> Movies { get; set; }
